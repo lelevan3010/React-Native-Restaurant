c76ff118a215a75faa269cad7fb5801bcc80f774
Van Nguyen <93vannd@gmail.com>
2020-02-23 12:16:28 +0200

    added loading spinner

--- views/Home.js
+++ views/Home.js
@@ -1,57 +1,48 @@
 import React from 'react'
 import { View } from 'react-native'
-import { OptimizedFlatList } from 'react-native-optimized-flatlist' // render faster than FlatList
 import { globalStyles } from '../styles/global'
 import { db } from '../firebase/firebaseConfig'
 
-import ItemMenu from '../components/ItemMenu'
+import withSpinner from '../hoc/withSpinner'
+import ListingMenu from '../components/ListingMenu'
 
-
-export default class  Home extends React.Component {
-  constructor(props){
+const ListingMenuWithSpinner = withSpinner(ListingMenu)
+class Home extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
-      menu: []
+      isLoaded: false,
+      menu: [],
     }
   }
 
-  componentDidMount(){
-    let docRef = db.collection("menu")
+  componentDidMount() {
+    let docRef = db.collection('menu')
     const menuList = []
-    docRef.get().then((querySnapshot) => { // Use Arrow function for binding, or this.setState() won't work
+    docRef.get().then(querySnapshot => {
+      // Use Arrow function for binding, or this.setState() won't work
       querySnapshot.forEach(doc => {
-        const {title, price, ingridients, uri} = doc.data()
+        const { title, price, ingridients, uri } = doc.data()
         menuList.push({
           id: doc.id,
           title,
           ingridients,
           price,
-          uri
+          uri,
         })
       })
-      this.setState({menu: menuList})
-    });
+      this.setState({ isLoaded: true, menu: menuList })
+    })
   }
-  
 
-  render (){
-    return(
-    <View style={globalStyles.container}>
-      <OptimizedFlatList
-        showsVerticalScrollIndicator={false}
-        data={this.state.menu}
-        renderItem={({ item }) =>{
-        return(
-          <ItemMenu
-            onPress={() => navigation.navigate('ItemDetails')}
-            uri={item.uri}
-            title={item.title}
-            ingridients={item.ingridients}
-            price={item.price}
-          />
-        )}}
-        keyExtractor={item => item.id.toString()}
-      />
-    </View>)
+  render() {
+    const { isLoaded, menu } = this.state
+    return (
+      <View style={globalStyles.container}>
+        <ListingMenuWithSpinner isLoaded={isLoaded} menuData={menu} />
+      </View>
+    )
   }
 }
+
+export default Home
